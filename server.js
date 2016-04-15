6f3fa8769106ac01d8c64796c41a048365fb0146
Richard Pisoni <richard.pisoni@gmail.com>
2016-04-15 13:48:37 -0500

    Added index page and set up the server and gulp

---
+++ server.js
@@ -0,0 +1,11 @@
+var connect = require('connect');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+var app = connect();
+app.use(serveStatic('./src'));
+app.use(serveStatic('./src'));
+app.use(serveStatic('./node_modules'));
+
+console.log('Starting webserver on http://localhost:8080/');
+http.createServer(app).listen(8080);
